464c2fa2a3142f7460c1f4edbf1eda48e45e4c53
Noah Sehman <noah.sehman@gmail.com>
2024-01-07 16:44:48 -0700

    new page, other fun stuff you know what it is

--- assets/css/semiSecret.py
+++ assets/css/semiSecret.py
@@ -22,38 +22,53 @@ PINK = (255, 0, 255)
 PURPLE = (127, 0 ,255)
 GREY = (160, 160, 160)
 
-# 0, 1, 2, 3
-suits = ['clubs', 'hearts', 'diamonds', 'spades']
-# 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D
-vals = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
-valvals = ['A', 'B', 'C', 'D']
-deck = {}
-for suit in suits:
-    for val in vals:
-        cind = None
-        if vals.index(val) > 9:
-            cind = valvals[vals.index(val) - 10]
-        else:
-            cind = str(vals.index(val))
-        ind = str(suits.index(suit)) + cind
-        deck[ind] = val + " of " + suit
-
 
 class card():
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
 
+def start():
+    isStart = False
+    while not isStart:
+        
+        WIN.fill(GREY)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        pygame.display.flip()
 
-run = True
-while run:
+def main():
+    run = True
+    while run:
 
-    WIN.fill(GREY)
-    mox, moy = pygame.mouse.get_pos()
+        WIN.fill(GREY)
+        mox, moy = pygame.mouse.get_pos()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+        
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    # 0, 1, 2, 3
+    suits = ['clubs', 'hearts', 'diamonds', 'spades']
+    # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D
+    vals = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King']
+    valvals = ['A', 'B', 'C', 'D']
+    deck = {}
+    for suit in suits:
+        for val in vals:
+            cind = None
+            if vals.index(val) > 9:
+                cind = valvals[vals.index(val) - 10]
+            else:
+                cind = str(vals.index(val))
+            ind = str(suits.index(suit)) + cind
+            deck[ind] = val + " of " + suit
     
-
-    pygame.display.flip()
+    main()

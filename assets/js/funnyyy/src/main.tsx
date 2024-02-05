14c3a877f1f6964c17001292f7875adaebe98f64
Noah Sehman <noah.sehman@gmail.com>
2024-02-05 13:06:28 -0700

    gotta react to my surroundings

--- assets/js/funnyyy/src/main.tsx
+++ assets/js/funnyyy/src/main.tsx
@@ -2,12 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "../../../css/skill.css"
 
-function MainBoy () {
-  return <table></table>
+function THead () {
+  let fungi = [];
+  for (let i = 0; i < 9; i++) {
+    if(i > 0){
+      fungi.push(<th className='inf'>{i}</th>);
+    }
+    else {
+      fungi.push(<th className='inf' id='zerr'></th>);
+    }
+  }
+  return <><tr>{fungi}</tr></>
+}
+function STrow () {
+  let foongi = [];
+  for (let i = 0; i < 9; i++) {
+    if (i === 0) {
+      foongi.push(<td className='inf'>a</td>)
+    }
+    else if (i % 2 === 0) {
+      foongi.push(<td className='light'></td>)
+    }
+    else {
+      foongi.push(<td className='dark'></td>)
+    }
+  }
+  let fungu = [];
+  for (let i = 0; i < 9; i++) {
+    if (i === 0) {
+      fungu.push(<td className='inf'>b</td>)
+    }
+    else if (i % 2 === 1) {
+      fungu.push(<td className='light'></td>)
+    }
+    else {
+      fungu.push(<td className='dark'></td>)
+    }
+  }
+  return<>
+  <tr>{foongi}</tr>
+  <tr>{fungu}</tr>
+  </>
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MainBoy></MainBoy>
+    <table>
+      <THead></THead>
+      {Array(4).fill(null).map((x, i) => {return <STrow key={i}></STrow>})}
+    </table>
   </React.StrictMode>,
 )

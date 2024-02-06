a975867d29da645cf4b8ef63bc7a382ca84a135d
Noah Sehman <noah.sehman@gmail.com>
2024-02-05 20:11:31 -0700

    This sucks. I'm doing it anyway I guess

--- assets/js/funnyyy/src/main.tsx
+++ assets/js/funnyyy/src/main.tsx
@@ -6,42 +6,50 @@ function THead () {
   let fungi = [];
   for (let i = 0; i < 9; i++) {
     if(i > 0){
-      fungi.push(<th className='inf' style={{textAlign: 'center'}}>{i}</th>);
+      fungi.push(<th className='inf' style={{textAlign: 'center'}} key={i}>{i}</th>);
     }
     else {
-      fungi.push(<th className='inf' id='zerr'></th>);
+      fungi.push(<th className='inf' id='zerr' key={i}></th>);
     }
   }
-  return <><tr>{fungi}</tr></>
+  return <><thead><tr>{fungi}</tr></thead></>
 }
+const lame = 'abcdefgh';
+const rs = lame.split('');
+let cr = 0;
+console.log(rs)
 function STrow () {
   let foongi = [];
   for (let i = 0; i < 9; i++) {
     if (i === 0) {
-      foongi.push(<td className='inf'>a</td>)
+      let tkey = String(cr) + String(i);
+      foongi.push(<td className='inf' key={tkey}>{rs[cr]}</td>);
+      cr++;
     }
     else if (i % 2 === 0) {
-      foongi.push(<td className='light'></td>)
+      foongi.push(<td className='light'></td>);
     }
     else {
-      foongi.push(<td className='dark'></td>)
+      foongi.push(<td className='dark'></td>);
     }
   }
   let fungu = [];
   for (let i = 0; i < 9; i++) {
     if (i === 0) {
-      fungu.push(<td className='inf'>b</td>)
+      let fkey = String(cr) + String(i);
+      fungu.push(<td className='inf' key={fkey}>{rs[cr]}</td>);
+      cr++;
     }
     else if (i % 2 === 1) {
-      fungu.push(<td className='light'></td>)
+      fungu.push(<td className='light'></td>);
     }
     else {
-      fungu.push(<td className='dark'></td>)
+      fungu.push(<td className='dark'></td>);
     }
   }
   return<>
-  <tr>{foongi}</tr>
-  <tr>{fungu}</tr>
+  <tr key="foongi">{foongi}</tr>
+  <tr key="fungu">{fungu}</tr>
   </>
 }
 
@@ -49,7 +57,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <table>
       <THead></THead>
-      {Array(4).fill(null).map((x, i) => {return <STrow key={i}></STrow>})}
+      <tbody>{Array(4).fill(null).map((x, i) => {return <STrow key={i}></STrow>})}</tbody>
     </table>
   </React.StrictMode>,
-)
+);

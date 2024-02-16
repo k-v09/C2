0d1913d9139726190034e102ab32b9c9380493a6
Noah Sehman <noah.sehman@gmail.com>
2024-02-16 16:43:45 -0700

    am I my mental stability? cause I'm inconsistently here wOoOoOoAh

--- assets/js/funnyyy/src/main.tsx
+++ assets/js/funnyyy/src/main.tsx
@@ -2,62 +2,65 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import "../../../css/skill.css"
 
-function THead () {
+
+const ls = "abcdefgh".split("");
+function THfoot () {
   let fungi = [];
   for (let i = 0; i < 9; i++) {
     if(i > 0){
-      fungi.push(<th className='inf' style={{textAlign: 'center'}} key={i}>{i}</th>);
+      fungi.push(<th className='inf' style={{textAlign: 'center'}} key={i}>{ls[i-1]}</th>);
     }
     else {
-      fungi.push(<th className='inf' id='zerr' key={i}></th>);
+      fungi.push(<th className='inf' id='zerr' key={i} style={{}}></th>);
     }
   }
-  return <><thead><tr>{fungi}</tr></thead></>
+  return <><tfoot><tr>{fungi}</tr></tfoot></>
 }
-const lame = 'abcdefgh';
-const rs = lame.split('');
-let cr = 0;
-console.log(rs)
-function STrow () {
-  let foongi = [];
-  for (let i = 0; i < 9; i++) {
-    if (i === 0) {
-      let tkey = String(cr) + String(i);
-      foongi.push(<td className='inf' key={tkey}>{rs[cr]}</td>);
-      cr++;
+
+function STrowz () {
+  let rowz = [];
+  for (let i = 1; i < 9; i++) {
+    let owz = [];
+    for (let j = 0; j < 9; j++) {
+      if (j === 0) {
+        owz.push(<td className='inf' style={{}}>{i}</td>);
+      }
+      else if (Math.abs(j - i) % 2 === 0) {
+        owz.push(<td className='light' style={{}}></td>);
+      }
+      else {
+        owz.push(<td className='dark' style={{}}></td>);
+      }
     }
-    else if (i % 2 === 0) {
-      foongi.push(<td className='light'></td>);
-    }
-    else {
-      foongi.push(<td className='dark'></td>);
-    }
+    rowz.push(<tr>{owz}</tr>);
   }
-  let fungu = [];
-  for (let i = 0; i < 9; i++) {
-    if (i === 0) {
-      let fkey = String(cr) + String(i);
-      fungu.push(<td className='inf' key={fkey}>{rs[cr]}</td>);
-      cr++;
-    }
-    else if (i % 2 === 1) {
-      fungu.push(<td className='light'></td>);
-    }
-    else {
-      fungu.push(<td className='dark'></td>);
-    }
-  }
-  return<>
-  <tr key="foongi">{foongi}</tr>
-  <tr key="fungu">{fungu}</tr>
-  </>
+  return <>{rowz}</>
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <table>
-      <THead></THead>
-      <tbody>{Array(4).fill(null).map((x, i) => {return <STrow key={i}></STrow>})}</tbody>
+      <tbody id='bud'><STrowz></STrowz></tbody>
+      <THfoot></THfoot>
     </table>
   </React.StrictMode>,
 );
+
+class Piece {
+  public color: string;
+  public row: number;
+  public col: number;
+  public type: string;
+  
+  constructor (color: string, row: number, col: number) {
+    this.color = color;
+    this.row = row;
+    this.col = col;
+    this.type = "Basic";
+  }
+}
+
+const ctab = document.getElementById("bud");
+console.log(ctab);
+//const pieces = {pawn: "p", knight: "n", bishop: "b", rook: "r", queen: "q", king: "k"}
+
